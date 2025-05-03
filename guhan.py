49a7257b3c740e708a0ce8d6d3e42904bb6116f3
guhan <vguhan063@gmail.com>
2025-05-03 11:52:15 +0530

    update the code in the line number 5

--- guhan.py
+++ guhan.py
@@ -1,7 +1,11 @@
 import pyvisa
+import time
 
 rm = pyvisa.ResourceManager()
+voa = rm.open_resource("GPIB0::20::INSTR")
 
-voa = rm.open_resource("g")
+bert = rm.open_resource("COM1")
+bert.baud_rate = 115200
 
+time.sleep(1)
 voa.query("read att values")
